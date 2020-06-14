84e80989b0c3bd784a743896f2f445cb6acd590d
bintemasood <humairamasood19@gmail.com>
2020-06-14 04:56:18 -0700

    Practise of Javascript with SMIT

---
+++ app.js
@@ -0,0 +1,187 @@
+ //CHAPTER#1 =========="ALERTS" ==========STARTS
+ //1.
+/*alert("Welcome to The English Luggagge");
+//2.
+alert("Error! Please enter a valid password.");
+//3.
+alert("Welcome to JS Land...\nHappy Coding!");
+//4.
+alert("Welcome to JS Land...")
+alert("Happy Coding!\n  Prevent this page from creating additional dialogs.");
+//5.
+//console.log("Hello... I can run JS through my web browser's console");
+
+
+//CHAPTER#2 =========="VARIABLES FOR STRINGS " ==========STARTS
+//1.
+var username = "abc";
+//2.
+var myName = "humaira";
+alert(myName);
+//3.
+var message = "Hello World";
+alert(message);
+//4.
+var name ="john Doe";
+var age ="15 years old";
+var course = "Certified Moblie Application Development";
+alert(name);
+alert(age);
+alert(course);
+//5.
+var  pizza = "pizza\npizz\npiz\npi\np";
+alert(pizza);
+//6.
+var email1 = "My email address is ";
+var email2 = "example@example.com";
+alert(email1 + email2);
+//7.
+var book1 = "I am trying to learn from the Book ";
+var book = "A smarter way to learn JavaScript";
+alert(book1 + book);
+//8.
+var txt = "Yah! I can write HTML content through JavaScript<br>";
+document.write(txt);
+//9.
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+//CHAPTER#3 =========="VARIABLES FOR NUMBERS" ==========STARTS
+1.
+var age = "I am 15 years old";
+alert(age);
+//2.
+
+//3.
+var birthYear = "My birth year is 1990 \nData type of my declared variable in number";
+alert(birthYear);
+//4.
+
+var visitorName = "<b>John Doe </b>";
+var prouduct = "<b>T-shirts (s) </b>";
+var quantity = "<b>5 </b>";
+document.write(visitorName + " " + "ordered" 
++ quantity + prouduct + "on XYZ Clothing Store.<br><br>" ) 
+
+//CHAPTER#4 =========="LEGAL AND ILLEGAL" ==========STARTS
+/*1. Declare 3 variables in one statement.
+alert total = cost + profit ;
+2. Declare 5 legal & 5 illegal variable names.
+legal                           illegal
+1) firstname                    1)first name
+2) fist_name                    2)20firstname
+3) first-name                   3)alert
+4) firstName                    4)not be a keyword
+5) firstname20                  5)not in quotes*/
+
+//3.
+/*var text = ("<b>Rules for Naming JS Variables</b><br><br>");
+document.write(text );
+
+var text1 = ("Variable names can only contain numbers,$,and_.For example $my_1stVariable<br>Variables must begin with a letter, or_. For example $name, _name or name<br>Variable names are case sensitive.<br>Variable names should not be JS keywords.<br></br>")
+document.write(text1);
+
+//CHAPTER#5 =========="MATH EXPRESSIONS" ==========STARTS
+
+//1.
+var a = 3;
+var b = 5;
+var c = a + b;
+document.write("Sum of " + a + " and " + b + " is " + c );
+
+//2.=====subtraction======
+var a = 13;
+var b = 5;
+var c = a - b;
+document.write("<br>Difference of " + a + " and " + b + " is " + c);
+//  =====multiplication======
+var a = 4;
+var b = 2;
+var c = a * b;
+document.write("<br>Product of " + a + " and " + b + " is " + c);
+//  =====division======
+var a = 16;
+var b = 2;
+var c = a / b;
+document.write("<br>Quotient of " + a + " and " + b + " is " + c);
+//  =====modulus======
+var a = 16;
+var b = 2;
+var c = a % b;
+document.write("<br>Modulus of " + a + " and " + b + " is " + c);
+
+//3.
+//4.
+var ticket = 600;
+var buy = 5;
+var total = ticket * buy;
+document.write("<br>Total cost to buy 5 tickets to a movie is " + total + "PKR<br><br>");
+
+//5.
+var u = +prompt("Enter Your Number");
+for (var s = 1; s < 11; s++){
+    document.write(u + "x" + s + "=" + u*s + "<br>");
+}
+
+//CHAPTER#6-9 =========="MATH EXPRESSIONS" ==========STARTS
+//3.
+var userName = prompt("Enter your Name");
+alert("Welcome " + userName + " Glad to see you!");
+//6.
+//(A)
+var subject1 = prompt("Enter subject 1");
+alert(subject1);
+var subject2 = prompt("Enter subject 2");
+var subject3 = prompt("Enter subject 3");
+//(b)
+var total = 100;
+//(c)
+var obtain1 = +prompt("enter your " + subject1 + " marks");
+//(d)
+var obtain2 = +prompt("enter your " + subject2 + " marks");
+var obtain3 = +prompt("enter your " + subject3 + " marks");
+var totObt = obtain1 + obtain2 + obtain3;
+var per1 = obtain1 / total * 100;
+var per2 = obtain2 / total * 100;
+var per3 = obtain3 / total * 100;
+var totPer = per1 + per2 + per3;
+//(e)
+document.write("<b>Subject</b><br>" + subject1 + "<br>" + subject2 + "<br>" + subject3);
+document.write("<br><b>Total</b><br>" + total + "<br>" + total + "<br>" + total + "<br>");
+document.write("<br><b>Obtained Marks</b><br>" + obtain1 + "<br>" + obtain2 + "<br>" + obtain3 + "<br>" + totObt);
+document.write("<br><b>Percentage</b><br>" + per1 + "<br>" + per2 + "<br>" + per3 + "<br>" + totPer);*/
+
+//CHAPTER#9-11 =========="USER INPUT & CONDITIONAL STATEMENT" ==========STARTS
+//1.
+/*var cityName = prompt("Enter Your City ");
+if( cityName = "karachi"){
+    document.write("Welcome to city of Lights!");
+}; 
+//2.
+var m = "male";
+var f = "female";
+var gender = prompt("Enter your gender");
+if(gender == m){
+    alert("Good Morning Sir")
+}else{
+    alert("Good Morning Ma'am")
+};
+
+//3.
+var r = "red";
+var g = "green";
+var y = "yellow";
+var signalColor = prompt("enter signalcolor");
+if(signalColor == r ){
+    console.table("Must Stop")
+    if(signalColor == g){
+        console.table("Move Now")
+        if (signalColor == y){
+            console.table("Ready to Move")
+        }
+    }
+};*/
+
+//CHAPTER#12-13 =========="IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS" ==========STARTS
+//CHAPTER#14-16 =========="ARRAYS" ==========STARTS
+//CHAPTER#17-20 =========="ARRAYS AND LOOP" ==========STARTS
